21f67538ad4ce21d86ea6ac1101f3ae39189fc2d
Luiz Aguiar <luiz.aguiar@gmail.com>
2017-10-07 23:25:53 +0200

    Initial commit

---
+++ src/main/kotlin/com/kryptonkoins/KryptonCoinsApplication.kt
@@ -0,0 +1,11 @@
+package com.kryptonkoins
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class KryptonKoinsApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(KryptonKoinsApplication::class.java, *args)
+}
