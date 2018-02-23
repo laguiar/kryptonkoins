8c458dbfe38240638df771ff9344305e9e9cc3f2
Luiz Aguiar <luiz.aguiar@gmail.com>
2018-02-23 11:03:19 +0100

    added an extension method

--- src/main/kotlin/com/kryptonkoins/web/dto/KryptonDTO.kt
+++ src/main/kotlin/com/kryptonkoins/web/dto/KryptonDTO.kt
@@ -1,6 +1,7 @@
 package com.kryptonkoins.web.dto
 
 import com.kryptonkoins.domain.Koin
+import com.kryptonkoins.domain.Krypton
 import org.springframework.validation.annotation.Validated
 import java.math.BigDecimal
 import javax.validation.constraints.NotEmpty
@@ -19,4 +20,11 @@ data class SellDTO(
     @NotNull val shares: BigDecimal,
     @NotNull val price: BigDecimal,
     val fees: BigDecimal = BigDecimal("0.0000000000")
+)
+
+fun BuyDTO.toKrypton(): Krypton = Krypton(
+    koin = this.koin,
+    shares = this.shares,
+    buy = this.price,
+    buyFees = this.fees
 )
